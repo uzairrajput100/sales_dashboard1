63c1028ab8a4181b6cfca8ec554ddef0ba0dc143
Uzair Ahmed <uzairrajput100@gmail.com>
2024-06-19 11:42:22 +0400

    Add files via upload

---
+++ case_study_adidas.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import streamlit as st
+import datetime
+import plotly.express as px
+from PIL import Image
+import plotly.graph_objects as go
+
+df = pd.read_excel('AdidasSalesdata.xlsx')
+st.set_page_config(layout='wide')
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+image = Image.open('adidas_logo.png')
+
+col1, col2 = st.columns([0.1,0.9])
+with col1:
+    st.image(image, width=100)
+
+html_title = """"
+<style>.title-test {font-weight"bold;
+padding:5px;
+border-radius:6px
+}
+</style>
+<center><h1 class= "title-test">Adidas Interactive Sales Dashboard</h1></center>"""
+with col2:
+    st.markdown(html_title, unsafe_allow_html=True)
+
+col3, col4, col5 = st.columns([0.1,0.45,0.45])
+with col3:
+    box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
+    st.write(f"Last updated by:  \n{box_date}")
+
+with col4:
+    fig = px.bar(df, "Retailer", y= "Total Sales", labels= {"Total Sales" : "Total Sales {$}"},
+                 title="Total Sales by Retailer", hover_data=["Total Sales"],
+                 height=500, template="gridon")
+    st.plotly_chart(fig, use_container_width=True)
+
+_, view1, dwn1, view2, dwn2 = st.columns([0.15,0.20,0.20,0.20,0.20])
+with view1:
+    expander = st.expander("Retailer wise Sales")
+    data = df[["Retailer", "Total Sales"]].groupby(by="Retailer")["Total Sales"].sum()
+    expander.write(data)
+with dwn1:
+    st.download_button('Get data', data= data.to_csv().encode('utf-8'),
+                       file_name="Retailer Sales.csv", mime="test/csv")
+df['Month_year'] = df['Invoice Date'].dt.strftime("%b '%y")
+result = df.groupby(by= df["Month_year"])["Total Sales"].sum().reset_index()
+with col5:
+    fig1 = px.line(result, x= "Month_year", y= "Total Sales", title="Total sales over time",
+                   template="gridon")
+    st.plotly_chart(fig1, use_container_width=True)
+
+with view2:
+    expander = st.expander("Monthly Sales")
+    data = result
+    expander.write(data)
+with dwn2:
+    st.download_button('Get data', data= result.to_csv().encode('utf-8'),
+                       file_name="Monthly Sales.csv", mime="test/csv")
+st.divider()
+
+result1 = df.groupby(by= "State")[["Total Sales", "Units Sold"]].sum().reset_index()
+fig3 = go.Figure()
+fig3.add_trace(go.Bar(x= result1['State'], y=result1['Total Sales'], name='Total Sales'))
+fig3.add_trace(go.Scatter(x=result1['State'], y= result1['Units Sold'], mode= "lines",
+                          name= "Units Sold", yaxis= "y2"))
+fig3.update_layout(title= "Total Sales and Units Sold by State",
+                   xaxis= dict(title="State"),
+                   yaxis= dict(title="Total Sales", showgrid= True),
+                   yaxis2 = dict(title= "Units Sold", overlaying= 'y', side= 'right'),
+                   template = "gridon", legend= dict(x=1,y=1))
+_, col6= st.columns([0.1,0.9])
+with col6:
+    st.plotly_chart(fig3, use_container_width=True)
+
+_, view3, dwn3= st.columns([0.5,0.45,0.45])
+with view3:
+    expander = st.expander("View Data for Sales by Units Sold")
+    expander.write(result1)
+with dwn3:
+    st.download_button('Get data', data= result1.to_csv().encode('utf-8'),
+                       file_name="Sales by units sold.csv", mime="test/csv")
+st.divider()
+
+_, col7 = st.columns([0.1,0.9])
+treemap = df[['Region', 'City', 'Total Sales']].groupby(by= ['Region', 'City'])['Total Sales'].sum().reset_index()
+
+def format_sales(value):
+    if value >= 0:
+        return '{:.2f} Lakh'.format(value / 1_000_00)
+treemap['Total Sales (Formatted)'] = treemap['Total Sales'].apply(format_sales)
+
+fig4 = px.treemap(treemap, path=['Region', 'City'], values ="Total Sales", hover_name='Total Sales (Formatted)',
+                  hover_data= ["Total Sales (Formatted)"], color= 'City', height=700, width=600)
+fig4.update_traces(textinfo='label+value')
+
+with col7:
+    st.subheader(':point_right: Total Sales by Region and City in Treemap')
+    st.plotly_chart(fig4, use_container_width=True)
+
+_, view4, dwn4= st.columns([0.5,0.45,0.45])
+with view4:
+    result2 = df[['Region', 'City', 'Total Sales']].groupby(by=['Region', 'City'])['Total Sales'].sum()
+    expander = st.expander("View Data for Total Sales by Region and City")
+    expander.write(result2)
+with dwn4:
+    st.download_button('Get data', data= result2.to_csv().encode('utf-8'),
+                       file_name="Sales by Region.csv", mime="test/csv")
+
+_, view5, dwn5= st.columns([0.5,0.45,0.45])
+with view5:
+    expander = st.expander("View Sales Raw Data")
+    expander.write(df)
+with dwn5:
+    st.download_button('Get Raw data', data= df.to_csv().encode('utf-8'),
+                       file_name="Sales Raw Data.csv", mime="test/csv")
+
+# Footer
+st.markdown(""" 
+---
+**Contact Us:** For any sales queries or support, feel free to reach out at +92 333 6611988 or email uzairrajput100@gmail.com
+""")
+
+st.divider()
